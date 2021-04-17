60403911afcfc89d9dbf4f8d53a23229ed4fb7b1
Wesley Bruno <engwesleybruno@gmail.com>
2021-04-17 20:30:27 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,11 +13,11 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		
+				
 		Seller obj = new Seller(1, "Wesley Bruno", "wbbs@gmail.com", sdf.parse("30/10/1992"), 8000.0, new Department(2, "Eletronics"));
 		
-		System.out.println(obj);
+		//Injeção de dependencia a partir de uma factory
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 
 	}
