4eae009f6795fd7a85c845e9d4b8b1db9e397567
Wesley Bruno <engwesleybruno@gmail.com>
2021-04-17 22:48:23 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,14 @@ public class Program {
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
 
+		System.out.println("\n=== TEST 5: seller Insert  ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Updated successfull!");
+
+		
+		
 	}
 
 }
