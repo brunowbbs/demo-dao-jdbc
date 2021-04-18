aa9ae77669769554eab062b38efd6eac8e785058
Wesley Bruno <engwesleybruno@gmail.com>
2021-04-17 21:07:46 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,20 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-				
-		Seller obj = new Seller(1, "Wesley Bruno", "wbbs@gmail.com", sdf.parse("30/10/1992"), 8000.0, new Department(2, "Eletronics"));
-		
-		//Injeção de dependencia a partir de uma factory
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
